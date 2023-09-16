e0726f90e44ce1b3ac72c34ae40b510cf7508635
codebyavinuk9 <avijit.pateriya@nuk.in>
2023-09-16 23:24:56 +0530

    code added

--- src/components/PageOne/PageOne.js
+++ src/components/PageOne/PageOne.js
@@ -1,16 +1,51 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./PageOne.css";
+const url = process.env.REACT_APP_API
 
-const PageOne = ({ onButtonClick}) => {
-
+const PageOne = () => {
+  const navigate = useNavigate();
+  const [empid, setEmpid] = useState("");
+  const [password, setPassword] = useState("");
+  const empIdHandler = (e) => {
+    setEmpid(e.target.value);
+  };
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { empid, password };
+    const userLogin = async () => {
+      try {
+        const userLog = await axios.post(
+          `${url}/api/login`,
+          data
+        );
+        alert("login successful");
+        localStorage.setItem("userId", userLog.data.empid);
+        localStorage.setItem(
+          "authToken",
+          JSON.stringify(userLog.data.authToken)
+        );
+        navigate("/pagetwo");
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    userLogin();
+    // navigate("/pageTwo")
+  };
   return (
     <main
       className="pt5 black-80 center"
       style={{ maxWidth: "40%", maxHeight: "30%", margin: "auto" }}
     >
-      <form className="measure">
-        <h2>Welcome! First things first...</h2>
-        <p style={{ color: "#C0C0C0" }}>You can always change them later.</p>
+      <form className="measure" onSubmit={handleSubmit} method="post">
+        <h2>Welcome! To Nuk9, First things first...</h2>
+        <p style={{ color: "#C0C0C0" }}>Your Wish We Deliver.</p>
         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
           <div className="mt3">
             <label
@@ -23,15 +58,18 @@ const PageOne = ({ onButtonClick}) => {
             <input
               className="f6 br2 ph3 pv2 mb2 dib black w-100"
               type="text"
-              name="full-name"
+              name="empid"
+              value={empid}
               id="full-name"
               size="30"
-              placeholder="Your ID"
+              required
+              placeholder="Please Enter Your Employee ID"
               style={{
                 borderStyle: "solid",
                 borderWidth: "1px",
                 borderColor: "#EAEEF5",
               }}
+              onChange={empIdHandler}
             />
           </div>
           <div className="mv3">
@@ -44,21 +82,24 @@ const PageOne = ({ onButtonClick}) => {
             </label>
             <input
               className="f6 br2 ph3 pv2 mb2 dib black w-100"
-              type="text"
-              name="display-name"
+              type="password"
+              name="password"
+              value={password}
+              required
               id="display-name"
-              placeholder="Password"
+              placeholder="Enter Your Password"
               style={{
                 borderStyle: "solid",
                 borderWidth: "1px",
                 borderColor: "#EAEEF5",
               }}
+              onChange={passwordHandler}
             />
           </div>
        
         </fieldset>
         <div>
-          <input
+          <button
             className="f6 grow br2 ph3 pv2 mb2 dib white"
             style={{
               borderStyle: "none",
@@ -66,9 +107,9 @@ const PageOne = ({ onButtonClick}) => {
               backgroundColor: "#664DE5",
             }}
             type="submit"
-            value="Login"
-            onClick={() => onButtonClick("pagetwo")}
-          />
+            value="Login">
+              Login
+            </button>
         </div>
       </form>
     </main>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+const root = document.getElementById('root');
+const reactRoot = createRoot(root);
+reactRoot.render(
+  <Router>
     <App />
+  </Router>
 );
